bf34df7ca51f68c26776d759778e0b3ed38029ae
Philip Hoffman <95146733+cubt85iz@users.noreply.github.com>
2025-04-29 22:58:13 -0400

    Updates build.ps1 to support building multiple ignition files

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,18 +1,39 @@
-# Install dependencies
-just install-deps
+# Pull the latest image
+docker pull -q quay.io/coreos/butane:release
 
-# Configure if not previously executed
-if (!(Test-Path ".generated\config.bu"))
-{
-  just configure
+# Search for butane files
+$files = @{}
+Get-ChildItem config -Filter "*.txt" -Recurse | ForEach-Object {
+
+  # Determine relative path to file
+  $relativePath =  $_ | Resolve-Path -Relative
+  
+  # Calculate depth
+  $depth = ($relativePath.ToCharArray() | Where-Object {
+    $_ -eq '\'
+  }).Count - 2
+
+  # Add values to dictionary
+  $files[$relativePath] = $depth
 }
 
-if (Test-Path(".generated"))
-{
-  Push-Location ".generated"
+# Sort files in descending order by value (depth); ascending order by key (path)
+$sortedFiles = $files.GetEnumerator() | Sort-Object @{Expression="Value"; Ascending=$false}, @{Expression="Key"; Ascending=$true}
 
-  # Build primary configuration
-  butane -p -d . -o config.ign config.bu
+# Iterate over sorted files and generate ignition files.
+$sortedFiles | ForEach-Object {
+  $file = .generated\$_.Key
+  $parent = Split-Path $file
 
-  Pop-Location
+  # Create parent directory for 
+  if (!(Test-Path "$parent")) {
+    New-Item -ItemType Directory -Path $parent
+  }
+
+  if ( $_.Value -eq 0 ) {
+    $_.Key | docker run --rm -i -v .:/files --workdir /files quay.io/coreos/butane:release --pretty --files-dir . > $file
+  }
+  else {
+    $_.Key | docker run --rm -i -v .:/files --workdir /files quay.io/coreos/butane:release --pretty --files-dir .generated > $file
+  }
 }
